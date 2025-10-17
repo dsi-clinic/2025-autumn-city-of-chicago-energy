9a61112135132361c356bde5aaf4c0366ec726bc
Alejandro <aorellana0412@gmail.com>
2025-10-17 15:16:20 -0500

    ran uv run pre-commit run --all-files in terminal to pass the lint for pull request

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,15 +1,21 @@
 from pathlib import Path
-import pandas as pd
+
 import numpy as np
+import pandas as pd
+
+
 def testing_loader():
     return print("found data loader file!")
 
+
 def loading_data():
-    target_folder="src"
+    target_folder = "src"
     current = Path.cwd()
     while current.name != target_folder:
         if current.parent == current:
-            raise FileNotFoundError(f"Could not find folder named '{target_folder}' above {Path.cwd()}")
+            raise FileNotFoundError(
+                f"Could not find folder named '{target_folder}' above {Path.cwd()}"
+            )
         current = current.parent
 
     path = current / "data" / "chicago_energy_benchmarking"
@@ -19,7 +25,7 @@ def loading_data():
     for file in path.rglob("*.csv"):
         print("Reading:", file)
         load_dfs.append(pd.read_csv(file))
-    full_df = pd.concat(load_dfs, axis=0, join='outer', ignore_index=True)
+    full_df = pd.concat(load_dfs, axis=0, join="outer", ignore_index=True)
     full_df = full_df.sort_values(by="Data Year", ascending=True)
 
     str_cols = [
@@ -45,8 +51,8 @@ def loading_data():
         "Weather Normalized Site EUI (kBtu/sq ft)",
         "Weather Normalized Source EUI (kBtu/sq ft)",
         "Total GHG Emissions (Metric Tons CO2e)",
-        "GHG Intensity (kg CO2e/sq ft)",    
-        "Water Use (kGal)",                 
+        "GHG Intensity (kg CO2e/sq ft)",
+        "Water Use (kGal)",
     ]
 
     full_df[str_cols] = full_df[str_cols].astype(str)
@@ -58,7 +64,7 @@ def loading_data():
                 .astype(str)
                 .str.replace(",", "", regex=False)
                 .str.strip()
-                .replace({"": np.nan})     
+                .replace({"": np.nan})
                 .astype("float64", errors="ignore")
             )
     return full_df

d75141375961afffbfd9f320266c745b1aba05cb
Alejandro <aorellana0412@gmail.com>
2025-11-11 23:13:29 -0600

    adding more information in introduction

--- dashboard/Introduction.py
+++ dashboard/Introduction.py
@@ -108,12 +108,38 @@ with col1:
     st.markdown(
         "The final dataframe filters dowe data to only have concurrent buildings for 10 year span"
     )
+    st.markdown("#### Definitions")
+    st.markdown(
+        "- **Chicago Energy Rating:** The zero-to-four-star Chicago Energy Rating assigned to the building in the shown Data Year. A building with zero stars did not submit a report, or did submit a report but was missing required information. All other buildings receive between one and four stars, with four stars reflecting the highest performance. Every building receives a Chicago Energy Rating Placard with this rating, which must be posted in a prominent location at the building. The rating must also be shared at the time of listing the building for sale or for lease. For more information, visit: www.ChicagoEnergyRating.org This column was added for the 2018 Data Year. It is blank for previous years."
+    )
+    st.markdown(
+        "- **ENERGY STAR Score**: 1-100 rating that assesses a property’s overall energy performance, based on national data to control for differences among climate, building uses, and operations. A score of 50 represents the national median.*"
+    )
+    st.markdown(
+        "- **Exempt From Chicago Energy Rating:** Shows whether the building is subject to the Chicago Energy Rating Ordinance. Some properties are required to submit energy benchmarking reports but are not subject to the requirements of the Chicago Energy Rating program. These buildings do not receive a Chicago Energy Rating, typically due to technical reasons. This column was added for the 2018 Data Year. It is blank for previous years."
+    )
+
 with col2:
     st.image(
         "../data/image/Chicago_River_Aerial.jpg",
         caption="Chicago River Aerial View",
         use_container_width=True,
     )
+
+with st.expander("Timeline of Reported Data"):
+    st.markdown("""
+    - [**2014** Data Reported in 2015](https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking-2014-Data-Reported-in-/tepd-j7h5/about_data)
+    - [**2015** Data Reported in 2016](https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking-2015-Data-Reported-in-/ebtp-548e/about_data)
+    - [**2016** Data Reported in 2017](https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking-2016-Data-Reported-in-/fpwt-snya/about_data)
+    - [**2017** Data Reported in 2018](https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking-2017-Data-Reported-in-/j2ev-2azp/about_data)
+    - [**2018** Data Reported in 2019 *(First year with Chicago Energy Rating)*](https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking-2018-Data-Reported-in-/m2kv-bmi3/about_data)
+    - [**2019** Data Reported in 2020](https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking-2019-Data-Reported-in-/jn94-it7m/about_data)
+    - [**2020** Data Reported in 2021](https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking-2020-Data-Reported-in-/ydbk-8hi6/about_data)
+    - [**2021** Data Reported in 2023](https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking-2021-Data-Reported-in-/gkf4-txtp/about_data)
+    - [**2022** Data Reported in 2023](https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking-2022-Data-Reported-in-/mz3g-jagv/about_data)
+    - [**2023** Data Reported in 2024](https://data.cityofchicago.org/Environment-Sustainable-Development/Chicago-Energy-Benchmarking-2023-Data-Reported-in-/3a36-5x9a/about_data)
+    """)
+
 ######################################################
 
 st.divider()
--- utils/dashboard_utils.py
+++ utils/dashboard_utils.py
@@ -18,6 +18,7 @@ def apply_page_config() -> None:
     """Standardizing page apperance"""
     st.set_page_config(
         page_title="Chicago Energy Dashboard",
+        layout="wide",
         initial_sidebar_state="expanded",
     )
 
